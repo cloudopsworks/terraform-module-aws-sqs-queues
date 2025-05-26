96d743a96017e4eb7731fc444f4f2abd9ba74cb1
Cristian Beraha <cristian@cloudops.works>
2025-05-26 12:58:13 -0300

    Add KMS key and alias resources for SQS encryption
    Introduced `aws_kms_key` and `aws_kms_alias` resources to enable encryption for SQS queues using KMS. Updated SQS queue and policies to handle configurations dynamically with `try` for robustness. This enhances security and flexibility for managing encrypted queues.

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@
 #
 
 resource "aws_sqs_queue" "this" {
-  for_each                          = var.configs
+  for_each                          = try(var.configs.queues, {})
   name                              = try(each.value.name, "") != "" ? each.value.name : format("%s-%s-queue", try(each.value.name_prefix, each.key), local.system_name)
   fifo_queue                        = try(each.value.fifo.enabled, null)
-  max_message_size                  = try(each.value.max_message_size, null)
   fifo_throughput_limit             = try(each.value.fifo.throughput_limit, null)
+  max_message_size                  = try(each.value.max_message_size, null)
   content_based_deduplication       = try(each.value.deduplication, null)
   visibility_timeout_seconds        = try(each.value.visibility_timeout, null)
   message_retention_seconds         = try(each.value.message_retention, null)
--- policies.tf
+++ policies.tf
@@ -9,7 +9,7 @@
 
 data "aws_iam_policy_document" "sqs_policy" {
   for_each = {
-    for key, config in var.configs : key => config
+    for key, config in try(var.configs.queues, {}) : key => config
     if length(try(config.policies.sqs, {})) > 0
   }
   statement {
@@ -39,7 +39,7 @@ data "aws_iam_policy_document" "sqs_policy" {
 
 resource "aws_sqs_queue_policy" "this" {
   for_each = {
-    for key, config in var.configs : key => config
+    for key, config in try(var.configs.queues, {}) : key => config
     if length(try(config.policies.sqs, {})) > 0
   }
   queue_url = aws_sqs_queue.this[each.key].id
@@ -48,7 +48,7 @@ resource "aws_sqs_queue_policy" "this" {
 
 resource "aws_sqs_queue_redrive_allow_policy" "this" {
   for_each = {
-    for key, config in var.configs : key => config
+    for key, config in try(var.configs.queues, {}) : key => config
     if length(try(config.policies.redrive_allow, {})) > 0
   }
   queue_url            = aws_sqs_queue.this[each.key].id
@@ -57,7 +57,7 @@ resource "aws_sqs_queue_redrive_allow_policy" "this" {
 
 resource "aws_sqs_queue_redrive_policy" "this" {
   for_each = {
-    for key, config in var.configs : key => config
+    for key, config in try(var.configs.queues, {}) : key => config
     if length(try(config.policies.redrive, {})) > 0
   }
   queue_url      = aws_sqs_queue.this[each.key].id
